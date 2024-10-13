90c5d382ed4aa8c31aa14e3051e492735c5164b7
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2024-10-13 20:57:44 +0900

    Provê localização `pt-BR` a nível de aplicação.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import {
+  ApplicationConfig,
+  DEFAULT_CURRENCY_CODE,
+  LOCALE_ID,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
 import { routes } from './app.routes';
+
+registerLocaleData(localePt, 'pt-BR');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
   ],
 };
